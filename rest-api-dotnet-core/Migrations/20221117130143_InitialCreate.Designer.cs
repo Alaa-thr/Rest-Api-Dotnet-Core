5b8beb0b152aaad1abf9edb1032aaee5c7d0c500
DELL <thralae@gmail.com>
2022-12-20 11:44:55 +0100

    new updates

--- rest-api-dotnet-core/Migrations/20221117130143_InitialCreate.Designer.cs
+++ rest-api-dotnet-core/Migrations/20221117130143_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using rest_api_dotnet_core.Data;
 
 namespace rest_api_dotnet_core.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221117130143_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
